7d144cd6e4067b4e3e415f36e3301287e8094478
tshrsolanki <81178283+tshrsolanki@users.noreply.github.com>
2022-06-30 12:39:09 +0530

    Update Rank.jsx

--- src/comp/Rank.jsx
+++ src/comp/Rank.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const Rank = (props) => {
   return (
     <>
-      <div className="white f3 tc">{props.name}, your current rank is</div>
-      <div className="white f3 tc">#{props.rank}</div>
+      <div className="white f3 tc">{props.name}, you have detected</div>
+      <div className="white f3 tc">#{props.rank} images</div>
     </>
   );
 };
